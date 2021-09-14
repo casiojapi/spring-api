678d21293227cd674c016519a7b91792fcc10ef7
maximosan <maximosantoro@protonmail.com>
2021-09-14 11:57:00 -0300

    agrego modelo de usuario

---
+++ train/src/main/java/maximosan/train/model/User.java
@@ -0,0 +1,12 @@
+package maximosan.train.model;
+
+import java.time.LocalDate;
+import java.util.List;
+import sun.util.resources.LocaleData;
+
+public class User {
+    private String username;
+    private String name;
+    private LocalDate birthdate;
+    private List<Book> books;
+}
