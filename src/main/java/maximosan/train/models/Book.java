370c844674922463bda667f9baa3487209853684
maximosan <maximosantoro@protonmail.com>
2021-09-22 10:50:57 -0300

    armo modelo de Book en db

--- src/main/java/maximosan/train/models/Book.java
+++ src/main/java/maximosan/train/models/Book.java
@@ -1,24 +1,41 @@
 package maximosan.train.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-
-//@Entity
-//@Table(name = "Book")
+@Entity
 @Data
 @AllArgsConstructor
 public class Book {
-    private String genre;
-    private String author;
-    private String title;
-    private String image;
-    private String subtitle;
-    private String publisher;
-    private String year;
-    private Integer pages;
+    @Id
     private String isbn;
-    //@Id
-    //@GeneratedValue
-    //@Column(name = "title", nullable = false);
+
+    @Column(name = "genre", nullable = false)
+    private String genre;
+
+    @Column(name = "author", nullable = false)
+    private String author;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "image", nullable = false)
+    private String image;
+
+    @Column(name = "subtitle", nullable = false)
+    private String subtitle;
+
+    @Column(name = "publisher", nullable = false)
+    private String publisher;
+
+    @Column(name = "year", nullable = false)
+    private String year;
+
+    @Column(name = "pages", nullable = false)
+    private Integer pages;
+
+
 }
